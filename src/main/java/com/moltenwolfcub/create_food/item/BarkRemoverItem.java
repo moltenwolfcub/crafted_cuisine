1d178f008a8d93e34c990911153e6157aff9f0be
MoltenWolfCub <oliver@tomlins.org.uk>
2022-03-22 17:54:02 +0000

    changed BarkRemoverItem to use bark

--- src/main/java/com/moltenwolfcub/create_food/item/BarkRemoverItem.java
+++ src/main/java/com/moltenwolfcub/create_food/item/BarkRemoverItem.java
@@ -4,12 +4,17 @@ import com.moltenwolfcub.create_food.init.ModItems;
 import com.moltenwolfcub.create_food.item.util.ItemBase;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.Level;
 
@@ -32,13 +37,12 @@ public class BarkRemoverItem extends ItemBase {
 
         BlockState strippedBlockstate = AxeItem.getAxeStrippingState(blockstate);
         if (strippedBlockstate != null) {
+            level.playSound(player, blockpos, SoundEvents.AXE_STRIP, SoundSource.BLOCKS, 1.0F, 1.0F);
 
             if (!level.isClientSide) {
-                ItemStack itemstack = new ItemStack(ModItems.CINNAMON.get());
-                ItemEntity itementity = new ItemEntity(level, blockpos.getX(), blockpos.getY(), blockpos.getZ(), itemstack);
 
                 level.setBlock(blockpos, strippedBlockstate, 11);
-                level.addFreshEntity(itementity);
+                spawnBarkItem(blockstate, level, blockpos);
 
                 if (player != null) {
                     context.getItemInHand().hurtAndBreak(1, player, (thing) -> {
@@ -50,6 +54,41 @@ public class BarkRemoverItem extends ItemBase {
         } else{
             return InteractionResult.PASS;
         }
+    }
+
+    public void spawnBarkItem(BlockState state, Level level, BlockPos pos){
+        ItemStack itemStack = getBarkAll(state);
+        if (itemStack != null){
+            ItemEntity itemEntity = new ItemEntity(level, pos.getX(), pos.getY(), pos.getZ(), itemStack);
+            level.addFreshEntity(itemEntity);
+        }
+    }
+
+    /**
+    * Returns an {@link ItemStack} of the block's corresponding bark if not an {@link ItemStack} of AIR
+    */
+    public ItemStack getBarkAll(BlockState blockState){
+        return getBark(blockState);
+    }
+    public ItemStack getBark(BlockState blockState){
+        Block blockStateblock = blockState.getBlock();
+        if (blockStateblock == Blocks.OAK_LOG) {return new ItemStack(ModItems.OAK_BARK.get());}
+        else if (blockStateblock == Blocks.OAK_WOOD) {return new ItemStack(ModItems.OAK_BARK.get());}
+        else if (blockStateblock == Blocks.BIRCH_LOG) {return new ItemStack(ModItems.BIRCH_BARK.get());}
+        else if (blockStateblock == Blocks.BIRCH_WOOD) {return new ItemStack(ModItems.BIRCH_BARK.get());}
+        else if (blockStateblock == Blocks.SPRUCE_LOG) {return new ItemStack(ModItems.SPRUCE_BARK.get());}
+        else if (blockStateblock == Blocks.SPRUCE_WOOD) {return new ItemStack(ModItems.SPRUCE_BARK.get());}
+        else if (blockStateblock == Blocks.JUNGLE_LOG) {return new ItemStack(ModItems.JUNGLE_BARK.get());}
+        else if (blockStateblock == Blocks.JUNGLE_WOOD) {return new ItemStack(ModItems.JUNGLE_BARK.get());}
+        else if (blockStateblock == Blocks.ACACIA_LOG) {return new ItemStack(ModItems.ACACIA_BARK.get());}
+        else if (blockStateblock == Blocks.ACACIA_WOOD) {return new ItemStack(ModItems.ACACIA_BARK.get());}
+        else if (blockStateblock == Blocks.DARK_OAK_LOG) {return new ItemStack(ModItems.DARK_OAK_BARK.get());}
+        else if (blockStateblock == Blocks.DARK_OAK_WOOD) {return new ItemStack(ModItems.DARK_OAK_BARK.get());}
+        else if (blockStateblock == Blocks.CRIMSON_STEM) {return new ItemStack(ModItems.CRIMSON_BARK.get());}
+        else if (blockStateblock == Blocks.CRIMSON_HYPHAE) {return new ItemStack(ModItems.CRIMSON_BARK.get());}
+        else if (blockStateblock == Blocks.WARPED_STEM) {return new ItemStack(ModItems.WARPED_BARK.get());}
+        else if (blockStateblock == Blocks.WARPED_HYPHAE) {return new ItemStack(ModItems.WARPED_BARK.get());}
+        else{return new ItemStack(Items.AIR);}
     }
     
 }
