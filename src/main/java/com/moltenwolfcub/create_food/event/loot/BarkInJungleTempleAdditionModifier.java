3c41b2dcda1cc1249ac121a2497378e15f6d3298
MoltenWolfCub <oliver@tomlins.org.uk>
2022-04-03 08:30:54 +0100

    changed how loot is added to jungle temples

--- src/main/java/com/moltenwolfcub/create_food/event/loot/BarkInJungleTempleAdditionModifier.java
+++ src/main/java/com/moltenwolfcub/create_food/event/loot/BarkInJungleTempleAdditionModifier.java
@@ -19,24 +19,21 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class BarkInJungleTempleAdditionModifier extends LootModifier {
 
     private final Item addition;
+    private final Float chance;
+    private final Integer count;
 
-    protected BarkInJungleTempleAdditionModifier(LootItemCondition[] conditionsIn, Item addition) {
+    protected BarkInJungleTempleAdditionModifier(LootItemCondition[] conditionsIn, Item addition, Float chance, Integer count) {
         super(conditionsIn);
         this.addition = addition;
+        this.chance = chance;
+        this.count = count;
     }
 
     @Nonnull
     @Override
     protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
-        generatedLoot.add(new ItemStack(addition, 1));
-        if (context.getRandom().nextFloat() > 0.3) {
-            generatedLoot.add(new ItemStack(addition, 1));
-        }
-        if (context.getRandom().nextFloat() > 0.2) {
-            generatedLoot.add(new ItemStack(addition, 1));
-        }
-        if (context.getRandom().nextFloat() > 0.1) {
-            generatedLoot.add(new ItemStack(addition, 1));
+        if (context.getRandom().nextFloat() < this.chance) {
+            generatedLoot.add(new ItemStack(addition, count));
         }
         return generatedLoot;
     }
@@ -46,15 +43,21 @@ public class BarkInJungleTempleAdditionModifier extends LootModifier {
         @Override
         public BarkInJungleTempleAdditionModifier read(ResourceLocation name, JsonObject object,
                 LootItemCondition[] conditionsIn) {
+
             Item addition = ForgeRegistries.ITEMS.getValue(new ResourceLocation(
                 GsonHelper.getAsString(object, "addition")));
-                return new BarkInJungleTempleAdditionModifier(conditionsIn, addition);
+            Float chance = GsonHelper.getAsFloat(object, "chance");
+            Integer count = GsonHelper.getAsInt(object, "count");
+
+                return new BarkInJungleTempleAdditionModifier(conditionsIn, addition, chance, count);
         }
 
         @Override
         public JsonObject write(BarkInJungleTempleAdditionModifier instance) {
             JsonObject json = makeConditions(instance.conditions);
             json.addProperty("addition", ForgeRegistries.ITEMS.getKey(instance.addition).toString());
+            json.addProperty("chance", instance.chance);
+            json.addProperty("count", instance.count);
             return json;
         }
         
