61d0f04c8b3bf45fa661619a28b94ee13381c063
MoltenWolfCub <oliver@tomlins.org.uk>
2022-05-11 15:56:20 +0100

    added block loot_table data generator

--- src/main/java/com/moltenwolfcub/cooks_kitchen/datagen/DataGenerators.java
+++ src/main/java/com/moltenwolfcub/cooks_kitchen/datagen/DataGenerators.java
@@ -17,5 +17,6 @@ public class DataGenerators {
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
 
         generator.addProvider(new ModRecipeProvider(generator));
+        generator.addProvider(new ModLootTableProvider(generator));
     }
 }
