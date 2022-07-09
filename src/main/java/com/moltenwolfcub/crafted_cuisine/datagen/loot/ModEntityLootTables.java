b5987758d003c073826db74700f0b338ad581c6e
MoltenWolfCub <oliver@tomlins.org.uk>
2022-07-09 10:52:30 +0100

    added temporary empty loottable to cloaks

--- src/main/java/com/moltenwolfcub/crafted_cuisine/datagen/loot/ModEntityLootTables.java
+++ src/main/java/com/moltenwolfcub/crafted_cuisine/datagen/loot/ModEntityLootTables.java
@@ -1,5 +1,14 @@
 package com.moltenwolfcub.crafted_cuisine.datagen.loot;
 
-public class ModEntityLootTables {
+import com.moltenwolfcub.crafted_cuisine.init.AllEntityTypes;
+
+import net.minecraft.data.loot.EntityLoot;
+import net.minecraft.world.level.storage.loot.LootTable;
+
+public class ModEntityLootTables extends EntityLoot {
     
+    @Override
+    protected void addTables() {
+        add(AllEntityTypes.CLOAK.get(), LootTable.lootTable());
+    }
 }
