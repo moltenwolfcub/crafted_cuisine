33560710336653927c6bf463f3dab142efffbbcb
MoltenWolfCub <oliver@tomlins.org.uk>
2022-08-29 19:24:32 +0100

    setup empty base fabric project

---
+++ FABRIC/src/main/java/com/moltenwolfcub/crafted_cuisine/CraftedCuisine.java
@@ -0,0 +1,13 @@
+package com.moltenwolfcub.crafted_cuisine;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CraftedCuisine implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("crafted_cuisine");
+
+	@Override
+	public void onInitialize() {
+	}
+}
