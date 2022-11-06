899961b6ec0082a351b2982f87f7701653f01449
MoltenWolfCub <oliver@tomlins.org.uk>
2022-11-06 14:46:33 +0000

    fixed ModCreativeTab name and registry

--- FABRIC/src/main/java/com/moltenwolfcub/crafted_cuisine/item/util/ModCreativeTabs.java
+++ FABRIC/src/main/java/com/moltenwolfcub/crafted_cuisine/item/util/ModCreativeTabs.java
@@ -1,18 +1,30 @@
 package com.moltenwolfcub.crafted_cuisine.item.util;
 
+import java.util.function.Supplier;
+
 import com.moltenwolfcub.crafted_cuisine.CraftedCuisine;
 import com.moltenwolfcub.crafted_cuisine.init.AllItems;
 
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.resources.ResourceLocation;
+import net.fabricmc.fabric.impl.item.group.ItemGroupExtensions;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
-//TODO rename class
 public class ModCreativeTabs {
 
-    public static final CreativeModeTab MAIN_CREATIVE_TAB = FabricItemGroupBuilder.build(
-        new ResourceLocation(CraftedCuisine.MODID, "main_group"),
-            () -> new ItemStack(AllItems.CARAMEL_BUCKET));
+    public static final CreativeModeTab MAIN_CREATIVE_TAB = register(CraftedCuisine.MODID, () -> new ItemStack(AllItems.CARAMEL_BUCKET));
+
+
+	public static CreativeModeTab register(String name, Supplier<ItemStack> icon) {
+		((ItemGroupExtensions) CreativeModeTab.TAB_BUILDING_BLOCKS).fabric_expandArray();
+
+		return new CreativeModeTab(CreativeModeTab.TABS.length - 1, name) {
+
+			@Override
+			public ItemStack makeIcon() {
+				return icon.get();
+			}
+
+		};
+	}
 }
 
