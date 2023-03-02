84ba5e650337562b5b5ce61247406f25591fbf70
MoltenWolfCub <oliver@tomlins.org.uk>
2023-03-02 16:59:29 +0000

    removed forge code
    FABRIC subfolder moved to the root folder

--- FORGE/src/main/java/com/moltenwolfcub/crafted_cuisine/init/AllWoodTypes.java
+++
@@ -1,7 +0,0 @@
-package com.moltenwolfcub.crafted_cuisine.init;
-
-import net.minecraft.world.level.block.state.properties.WoodType;
-
-public class AllWoodTypes {
-    public static WoodType CINNAMON = WoodType.create("cinnamon");
-}
--- FORGE/src/main/java/com/moltenwolfcub/crafted_cuisine/intergration/ModIntergrations.java
+++
@@ -1,9 +0,0 @@
-package com.moltenwolfcub.crafted_cuisine.intergration;
-
-import net.minecraftforge.fml.ModList;
-
-public class ModIntergrations {
-    public boolean getLoaded(String id){
-        return ModList.get().isLoaded(id);
-    }
-}
