7b6f8eb58f81a5398b7ce8cc7b6b2a26f2659707
MoltenWolfCub <oliver@tomlins.org.uk>
2023-03-12 08:32:45 +0000

    completed structure_set datagen

--- src/main/java/com/moltenwolfcub/crafted_cuisine/data/world/ModStructureSets.java
+++ src/main/java/com/moltenwolfcub/crafted_cuisine/data/world/ModStructureSets.java
@@ -2,10 +2,12 @@ package com.moltenwolfcub.crafted_cuisine.data.world;
 
 import com.moltenwolfcub.crafted_cuisine.CraftedCuisine;
 
+import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureSet;
 import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadStructurePlacement;
 import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadType;
@@ -15,10 +17,13 @@ public class ModStructureSets {
     public static final ResourceKey<StructureSet> BLACKSTONE_FORTRESS_KEY = registerKey("blackstone_fortress");
     
     public static void bootstrap(BootstapContext<StructureSet> bootstapContext) {
+        HolderGetter<Structure> structureRegistryLookup = bootstapContext.lookup(Registries.STRUCTURE);
+
+
         bootstapContext.register(
             BLACKSTONE_FORTRESS_KEY,
             new StructureSet(
-                holderGetter.getOrThrow(BuiltinStructures.DESERT_PYRAMID), //needs structures datagen first
+                structureRegistryLookup.getOrThrow(ModStructures.BLACKSTONE_FORTRESS_KEY),
                 (StructurePlacement)new RandomSpreadStructurePlacement(
                     48,
                     12,
