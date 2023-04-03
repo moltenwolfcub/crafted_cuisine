210fe868185daff4dc34ed37eb4ab0546be63279
MoltenWolfCub <oliver@tomlins.org.uk>
2023-04-03 17:37:05 +0100

    made ReusableCraftingItems with durability take damage
    when crafting with items that have durability they will take damage
    upon a craft.

--- src/main/java/com/moltenwolfcub/crafted_cuisine/item/util/ReusableCraftingItem.java
+++ src/main/java/com/moltenwolfcub/crafted_cuisine/item/util/ReusableCraftingItem.java
@@ -1,6 +1,8 @@
 package com.moltenwolfcub.crafted_cuisine.item.util;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.ItemStack;
 
 public class ReusableCraftingItem extends ItemBase {
 
@@ -11,6 +13,18 @@ public class ReusableCraftingItem extends ItemBase {
 
     public ReusableCraftingItem() {
         this.craftingRemainingItem = this.asItem();
+    }
+
+    @Override
+    public ItemStack getRecipeRemainder(ItemStack stack) {
+        if (stack.isDamageableItem() && stack.getDamageValue() < stack.getMaxDamage() - 1) {
+            ItemStack damaged = stack.copy();
+            Minecraft mc = Minecraft.getInstance();
+            damaged.hurt(1, mc.level.random, null);
+            return damaged;
+        }
+      
+        return ItemStack.EMPTY;
     }
     
 }
