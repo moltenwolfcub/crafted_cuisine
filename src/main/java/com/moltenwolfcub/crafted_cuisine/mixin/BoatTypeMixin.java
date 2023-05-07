a7fea0d3b048d47828aa18c6d2ddb417cbe0d418
MoltenWolfCub <oliver@tomlins.org.uk>
2023-05-07 13:50:10 +0100

    fixed world render errors
    the boat mixin was causing errors with world rendering and client
    interaction. Appears to be due to the AllBlocks.CINNAMON_PLANKS.
    temporarily changed it to null

--- src/main/java/com/moltenwolfcub/crafted_cuisine/mixin/BoatTypeMixin.java
+++ src/main/java/com/moltenwolfcub/crafted_cuisine/mixin/BoatTypeMixin.java
@@ -2,6 +2,7 @@ package com.moltenwolfcub.crafted_cuisine.mixin;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
@@ -13,24 +14,21 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.moltenwolfcub.crafted_cuisine.CraftedCuisine;
 import com.moltenwolfcub.crafted_cuisine.entity.ModBoatType;
-import com.moltenwolfcub.crafted_cuisine.init.AllBlocks;
 
 import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.level.block.Block;
 
 @Mixin(Boat.Type.class)
 public class BoatTypeMixin {
-
 	//thanks to https://github.com/nyuppo/fabric-boat-example for this
 
     @SuppressWarnings("InvokerTarget")
     @Invoker("<init>")
-    private static Boat.Type newType(String internalName, int internalId, Block planks, String name) {
+    private static Boat.Type newType(String internalName, int internalId, Block baseBlock, String name) {
         throw new AssertionError();
     }
-	
+
     @SuppressWarnings("ShadowTarget")
     @Shadow
     @Final
@@ -39,20 +37,19 @@ public class BoatTypeMixin {
 
     @SuppressWarnings("UnresolvedMixinReference")
     @Inject(method = "<clinit>", at = @At(
-		value = "FIELD",
+        value = "FIELD",
         opcode = Opcodes.PUTSTATIC,
         target = "Lnet/minecraft/world/entity/vehicle/Boat$Type;$VALUES:[Lnet/minecraft/world/entity/vehicle/Boat$Type;",
         shift = At.Shift.AFTER
-	))
+    ))
     private static void addCustomBoatType(CallbackInfo ci) {
-		CraftedCuisine.LOGGER.info("Existing Boat Types: "+Arrays.asList(BoatTypeMixin.$VALUES));
-        ArrayList<Boat.Type> types = new ArrayList<Boat.Type>(Arrays.asList(BoatTypeMixin.$VALUES));
+        List<Boat.Type> types = new ArrayList<Boat.Type>(Arrays.asList($VALUES));
         Boat.Type last = types.get(types.size() - 1);
 
-        Boat.Type cinnamon = newType("CINNAMON", last.ordinal() + 1, AllBlocks.CINNAMON_PLANKS, "cinnamon");
+        Boat.Type cinnamon = newType("CINNAMON", last.ordinal() + 1, null, "cinnamon");
         ModBoatType.CINNAMON = cinnamon;
         types.add(cinnamon);
 
-        BoatTypeMixin.$VALUES = types.toArray(new Boat.Type[0]);
+        $VALUES = types.toArray(new Boat.Type[0]);
     }
 }
