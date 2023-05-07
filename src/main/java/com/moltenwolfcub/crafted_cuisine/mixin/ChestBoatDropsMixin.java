87b8c80854015a32c66fef1a3c8f12394f60ac58
MoltenWolfCub <oliver@tomlins.org.uk>
2023-05-07 19:08:42 +0100

    added boat drops

---
+++ src/main/java/com/moltenwolfcub/crafted_cuisine/mixin/ChestBoatDropsMixin.java
@@ -0,0 +1,26 @@
+package com.moltenwolfcub.crafted_cuisine.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.moltenwolfcub.crafted_cuisine.entity.ModBoatType;
+import com.moltenwolfcub.crafted_cuisine.init.AllItems;
+
+import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.entity.vehicle.ChestBoat;
+import net.minecraft.world.item.Item;
+
+@Mixin(ChestBoat.class)
+public class ChestBoatDropsMixin {
+
+    @Inject(method = "getDropItem", at = @At("HEAD"), cancellable = true)
+    public void getDropItem(CallbackInfoReturnable<Item> ci) {
+        Boat.Type type = ((Boat)(Object)this).getVariant();
+
+        if (type == ModBoatType.CINNAMON) {
+            ci.setReturnValue(AllItems.CINNAMON_CHEST_BOAT);
+        }
+    }
+}
